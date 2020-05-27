ed06b537996bf3ad5ff70ecc3b04fbb21e14aa02
jacky <clorisforcoding@gmail.com>
2020-05-27 13:00:16 +0800

    Initial commit

---
+++ datasource/src/main/java/cn/teamwang/datasource/DataApplication.java
@@ -0,0 +1,15 @@
+package cn.teamwang.datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jacky
+ * Date: 2020/5/23 23:18
+ */
+@SpringBootApplication
+public class DataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
+}
